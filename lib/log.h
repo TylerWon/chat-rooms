77c0f8a93a8f8dcd41fd3f5f18905ea980176ff0
Tyler Won <won.tyler1@gmail.com>
2025-08-16 19:21:59 -0700

    Set log level to error

--- lib/log.h
+++ lib/log.h
@@ -5,7 +5,7 @@
 #define LOG_LEVEL_WARN 2
 #define LOG_LEVEL_ERROR 3
 
-#define CURRENT_LOG_LEVEL LOG_LEVEL_INFO // Set desired log level
+#define CURRENT_LOG_LEVEL LOG_LEVEL_ERROR // Set desired log level
 
 #if CURRENT_LOG_LEVEL <= LOG_LEVEL_DEBUG
 #define LOG_DEBUG(fmt, ...) fprintf(stderr, "[DEBUG] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
@@ -19,25 +19,25 @@
 #if CURRENT_LOG_LEVEL <= LOG_LEVEL_INFO
 #define LOG_INFO(fmt, ...) fprintf(stderr, "[INFO] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #else
-#define LOG_DEBUG(fmt, ...) \
-    do                      \
-    {                       \
+#define LOG_INFO(fmt, ...) \
+    do                     \
+    {                      \
     } while (0) // Empty macro
 #endif
 
 #if CURRENT_LOG_LEVEL <= LOG_LEVEL_WARN
 #define LOG_WARN(fmt, ...) fprintf(stderr, "[WARN] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #else
-#define LOG_DEBUG(fmt, ...) \
-    do                      \
-    {                       \
+#define LOG_WARN(fmt, ...) \
+    do                     \
+    {                      \
     } while (0) // Empty macro
 #endif
 
 #if CURRENT_LOG_LEVEL <= LOG_LEVEL_ERROR
 #define LOG_ERROR(fmt, ...) fprintf(stderr, "[ERROR] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #else
-#define LOG_DEBUG(fmt, ...) \
+#define LOG_ERROR(fmt, ...) \
     do                      \
     {                       \
     } while (0) // Empty macro
